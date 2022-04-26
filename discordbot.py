62ebde1ce116df1bfaad6ba4abc9813e73c9ab3e
821055716195 <44452761+NurungjiBurger@users.noreply.github.com>
2022-04-26 22:12:27 +0900

    Update discordbot.py
    now can use exception DB

--- discordbot.py
+++ discordbot.py
@@ -32,6 +32,9 @@ UPDATE = "update"
 DELETE = "delete"
 SELECT = "select"
 
+LOG = "log"
+EXCEPTION = "exception"
+
 intents = discord.Intents.all()
 client = discord.Client()
 
@@ -43,17 +46,36 @@ guild_id = [961443814101360661]
 
 def table_check():
 
-    sql = """SHOW TABLES"""
+    sql = "SHOW TABLES like 'user'"
     cur.execute(sql)
     result = cur.fetchall()
 
     if result:
-        return True
+        True
     else:
-        sql = '''CREATE TABLE user ( user_id bigint NOT NULL PRIMARY KEY, cnt int(10) )'''
+        sql = "CREATE TABLE user ( user_id bigint NOT NULL PRIMARY KEY, cnt int(10) )"
         cur.execute(sql)
         con.commit()
-        sql = """CREATE TABLE log (  id int NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id bigint NOT NULL, time varchar(255), activity varchar(255), FOREIGN KEY (user_id) REFERENCES user (user_id) )"""
+
+    sql = "SHOW TABLES like 'exception'"
+    cur.execute(sql)
+    result = cur.fetchall()
+
+    if result:
+        True
+    else:
+        sql = "CREATE TABLE exception (  user_id bigint NOT NULL, activity varchar(255) )"
+        cur.execute(sql)
+        con.commit()
+
+    sql = "SHOW TABLES like 'log'"
+    cur.execute(sql)
+    result = cur.fetchall()
+
+    if result:
+        True
+    else:
+        sql = "CREATE TABLE log (  id int NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id bigint NOT NULL, time varchar(255), activity varchar(255), FOREIGN KEY (user_id) REFERENCES user (user_id) )"
         cur.execute(sql)
         con.commit()
 
@@ -86,6 +108,35 @@ def user_data(type, user_id, cnt):
         con.commit()
         return result
 
+def exception_data(type, user_id, activity):
+
+    if type == INSERT:
+        sql = "INSERT INTO exception (user_id, activity) VALUES (%s, %s)"
+        cur.execute(sql, (user_id, activity))
+        con.commit()
+        return None
+    elif type == UPDATE:
+        return None
+    elif type == DELETE:
+        if activity == '':
+            sql = "DELETE FROM exception where user_id = %s"
+            cur.execute(sql, user_id)
+        else:
+            sql = "DELETE FROM exception WHERE user_id = %s and activity = %s"
+            cur.execute(sql, (user_id, activity))
+        con.commit()
+        return None
+    elif type == SELECT:
+        if activity == '':
+            sql = "SELECT * FROM exception where user_id = %s"
+            cur.execute(sql, user_id)
+        else:
+            sql = "SELECT * FROM exception where user_id = %s and activity = %s"
+            cur.execute(sql, (user_id, activity))
+        result = cur.fetchall()
+        con.commit()
+        return result
+
 
 def log_data(type, user_id, time, activity, id):
 
@@ -97,8 +148,12 @@ def log_data(type, user_id, time, activity, id):
     elif type == UPDATE:
         return None
     elif type == DELETE:
-        sql = "DELETE FROM log WHERE user_id = %s and id = %s"
-        cur.execute(sql, (user_id, id))
+        if id == -1:
+            sql = "DELETE FROM log where user_id = %s"
+            cur.execute(sql, user_id)
+        else :
+            sql = "DELETE FROM log WHERE user_id = %s and id = %s"
+            cur.execute(sql, (user_id, id))
         con.commit()
         return None
     elif type == SELECT:
@@ -124,7 +179,6 @@ async def on_ready():
 @bot.event
 async def on_member_update(before, after):
 
-
     # 토요일 일요일 제외
     tms = datetime.now(timezone(timedelta(hours=9)))
     if tms.weekday() >= 5:
@@ -146,11 +200,61 @@ async def on_member_update(before, after):
             if len(before.activities) < len(after.activities):
                 msg = "{}님이 {} 활동을 시작했습니다.".format(before.name, after.activities[0].name)
                 await channel.send(msg)
-                tms = datetime.now(timezone(timedelta(hours=9)))
-                tm = str(tms)[:10] + " " + str(tms.time())[:8]
-                user_data(UPDATE, before.id, data[0][1]+1)
-                log_data(INSERT, before.id, tm, after.activities[0].name, 0)
+                edata = exception_data(SELECT, after.id, after.activities[0].name)
+                if edata:
+                    return
+                else:
+                    tms = datetime.now(timezone(timedelta(hours=9)))
+                    tm = str(tms)[:10] + " " + str(tms.time())[:8]
+                    user_data(UPDATE, before.id, data[0][1]+1)
+                    log_data(INSERT, before.id, tm, after.activities[0].name, 0)
 
+# 적발 예외 등록
+@slash.slash(name="EnrollExceptionActivity", description="적발 예외 활동을 등록합니다.", guild_ids=guild_id)
+async def enroll_except(ctx, activity: str):
+
+    udata = user_data(SELECT, ctx.author.id, 0)
+    if udata:
+        edata = exception_data(SELECT, ctx.author.id, activity)
+        if edata:
+            await ctx.send("{}님의 {}활동은 이미 예외로 등록되어있습니다.".format(ctx.author.name, activity))
+        else:
+            exception_data(INSERT, ctx.author.id, activity)
+            await ctx.send("{}님의 {}활동은 예외로 등록되었습니다.".format(ctx.author.name, activity))
+    else:
+        await ctx.send("{}님은 감시대상이 아닙니다.".format(ctx.author.name))
+
+# 적발 예외 목록
+@slash.slash(name="ExceptionActivity", description="적발 예외 활동을 보여줍니다.", guild_ids=guild_id)
+async def exception(ctx):
+
+    udata = user_data(SELECT, ctx.author.id, 0)
+    if udata:
+        edata = exception_data(SELECT, ctx.author.id, '')
+        catalog = create_msg(ctx.author.name, edata, EXCEPTION)
+
+        if catalog == '':
+            await ctx.send("{}님 예외 활동을 등록해주세요.".format(ctx.author.name))
+        else:
+            await ctx.send(catalog)
+    else:
+        await ctx.send("{}님은 감시대상이 아닙니다.".format(ctx.author.name))
+
+
+# 적발 예외 삭제
+@slash.slash(name="DeleteExceptionActivity", description="적발 예외 활동을 삭제합니다.", guild_ids=guild_id)
+async def delete_except(ctx, activity: str):
+
+    udata = user_data(SELECT, ctx.author.id, 0)
+    if udata:
+        edata = exception_data(SELECT, ctx.author.id, activity)
+        if edata:
+            exception_data(DELETE, ctx.author.id, activity)
+            await ctx.send("{}님의 {}활동은 예외에서 삭제되었습니다.".format(ctx.author.name, activity))
+        else:
+            await ctx.send("{}님의 {}활동은 예외로 등록되어있지 않습니다.".format(ctx.author.name, activity))
+    else:
+        await ctx.send("{}님은 감시대상이 아닙니다.".format(ctx.author.name))
   
 # 적발 횟수 출력
 @slash.slash(name="Count", description="적발 횟수를 출력 합니다.", guild_ids=guild_id)
@@ -164,23 +268,11 @@ async def my_cnt(ctx):
     else:
         await ctx.send("{}님은 감시대상이 아닙니다. 감시 대상으로 등록해주세요.".format(ctx.author.name))
 
-def create_log(author_name, data):
-
-    """
-    list1 = str(num+1) / time join to "." str.split()
-
-    "000님이 ".join(list.add( list1, list2 ))
-
-    list2 = activity / /n join to "활동을 시작했습니다." str.split('\n')
-
-
-    list1 = list(map(list.__add__, str(num+1) , list(map(lambda x: data[x][2], data) + " ")
-
-    (list(map(list.__add__, list(map(lambda x: data[x][2], data) + " "), list(map(lambda x: data[x][3], data)))))
-    """
-
-def create_log_msg(name, data):
+def create_msg(name, data, type):
+    if type == LOG:
         return "\n".join([f"{idx + 1}. {log[2]} {name}님이 {log[3]} 활동을 시작했습니다." for idx, log in enumerate(data)])
+    elif type == EXCEPTION:
+        return "\n".join([f"예외활동 {idx + 1}. {log[1]}" for idx, log in enumerate(data)])
 
 # 적발 로그 출력
 @slash.slash(name="Log", description="적발 로그를 출력 합니다.", guild_ids=guild_id)
@@ -190,7 +282,7 @@ async def my_log(ctx):
     udata = user_data(SELECT, ctx.author.id, 0)
     if udata:
         ldata = log_data(SELECT, ctx.author.id, 0, 0, 0)
-        log = create_log_msg(ctx.author.name, ldata)
+        log = create_msg(ctx.author.name, ldata, LOG)
 
         if log == '':
             await ctx.send("적발된 내용이 없습니다.")
@@ -199,7 +291,7 @@ async def my_log(ctx):
 
     # 감시 대상이 아닌경우
     else:
-        await ctx.send("{}님은 감시대상이 아닙니다. 감시 대상으로 등록해주세요.".format(ctx.author.name))    
+        await ctx.send("{}님은 감시대상이 아닙니다. 감시 대상으로 등록해주세요.".format(ctx.author.name))
 
 # 적발 로그 지우기
 @slash.slash(name="RemoveLog", description="적발로그를 삭제합니다.", guild_ids=guild_id)
@@ -214,7 +306,7 @@ async def remove_log(ctx, num):
                 log_data(DELETE, ctx.author.id, 0, 0, ldata[int(num)-1][0])
                 user_data(UPDATE, ctx.author.id, udata[0][1]-1)
                 await ctx.send("{}님의 {}번째 적발은 철회 되었습니다.".format(ctx.author.name, num))
-                log = create_log_msg(ctx.author.name, log_data(SELECT, ctx.author.id, 0, 0, 0))
+                log = create_msg(ctx.author.name, log_data(SELECT, ctx.author.id, 0, 0, 0), LOG)
                 if log == '':
                     await ctx.send("적발된 내용이 없습니다.")
                 else:
@@ -276,11 +368,19 @@ async def clear(ctx):
     data = user_data(SELECT, ctx.author.id, 0)
     if data:
         # 해당 감시 해제를 요청한 멤버를 감시 명단에서 제외
+
         ldata = log_data(SELECT, ctx.author.id, 0, 0, 0)
+        if ldata:
+            log_data(DELETE, ctx.author.id, 0, 0, -1)
+        """
         length = len(ldata)
         for num in length:
             log_data(DELETE, ctx.author.id, 0, 0, ldata[num][0])
+            """
         user_data(DELETE, ctx.author.id, 0)
+        edata = exception_data(SELECT, ctx.author.id, '')
+        if edata:
+            exception_data(DELETE, ctx.author.id, '')
         await ctx.send('{}님 감시를 해제합니다.'.format(ctx.author.name))
     else :
         await ctx.send('{}님은 이미 감시대상이 아닙니다.'.format(ctx.author.name))
